0064f63439f1cab519c468887f3f21e91687cf97
Arkaprava Bhattacharya <arkaprava181204@gmail.com>
2026-05-23 23:20:55 +0530

    Add files via upload

---
+++ EmailService.java
@@ -0,0 +1,34 @@
+package com.arkaprava.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendOtpEmail(
+            String toEmail,
+            String otp
+    ) {
+
+        SimpleMailMessage message =
+                new SimpleMailMessage();
+
+        message.setTo(toEmail);
+
+        message.setSubject(
+                "Your OTP Code"
+        );
+
+        message.setText(
+                "Your OTP is: " + otp
+        );
+
+        mailSender.send(message);
+    }
+}
